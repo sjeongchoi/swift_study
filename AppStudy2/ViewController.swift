f802fe83d194dfddd37172db38011ca4826baeb1
mabel <tandri6@gmail.com>
2018-06-28 21:34:00 +0900

    make ui

--- AppStudy2/ViewController.swift
+++ AppStudy2/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strLabel: UILabel!
+    @IBOutlet weak var currentNumber: UILabel!
+//    var currentNumber:
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,49 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btnClick(_ sender: Any) {
+        strLabel.text = "Change the Value"
+    }
+    
+    @IBAction func numZero(_ sender: Any) {
+    }
+    @IBAction func numOne(_ sender: Any) {
+    }
+    @IBAction func numTwo(_ sender: Any) {
+    }
+    @IBAction func numThree(_ sender: Any) {
+    }
+    @IBAction func numFour(_ sender: Any) {
+    }
+    @IBAction func numFive(_ sender: Any) {
+    }
+    @IBAction func numSix(_ sender: Any) {
+    }
+    @IBAction func numSeven(_ sender: Any) {
+    }
+    @IBAction func numEight(_ sender: Any) {
+    }
+    @IBAction func numNine(_ sender: Any) {
+    }
+    @IBAction func numDot(_ sender: Any) {
+    }
+    
+    @IBAction func doCompleteAction(_ sender: Any) {
+    }
+    @IBAction func doPlusActoin(_ sender: Any) {
+    }
+    @IBAction func doMinusAction(_ sender: Any) {
+    }
+    @IBAction func doMultipleAction(_ sender: Any) {
+    }
+    @IBAction func doDivideAction(_ sender: Any) {
+    }
+    @IBAction func doPercentAction(_ sender: Any) {
+    }
+    @IBAction func clearCurrentState(_ sender: Any) {
+    }
+    @IBAction func changePlusMinus(_ sender: Any) {
+    }
 }
 
