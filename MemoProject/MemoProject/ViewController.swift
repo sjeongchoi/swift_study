a57754e9e2d98790c13170c3d2feb2cf084ec611
mabel <tandri6@gmail.com>
2018-07-09 07:53:42 +0900

    url add

--- MemoProject/MemoProject/ViewController.swift
+++ MemoProject/MemoProject/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // https://medium.com/@shawnbierman/how-to-add-core-data-to-an-existing-xcode-8-and-swift-3-project-508781b3e1c4
+    //https://medium.com/@shawnbierman/how-to-add-core-data-to-an-existing-xcode-8-and-swift-3-project-508781b3e1c4
+    // https://www.youtube.com/watch?v=4flsWxFzOtg
+    // https://www.youtube.com/watch?v=0_0mi2Zl4HU
+    // https://www.youtube.com/watch?v=ukmBZq0Zkc8
+    // https://www.youtube.com/watch?v=cwnVFbeS2-4
+    //https://www.google.co.kr/search?rlz=1C5CHFA_enKR730KR730&ei=VrdAW7yvBcbs0gTRy5DABQ&q=swift+4+core+data+appdelegate+does+not+appear+&oq=swift+4+core+data+appdelegate+does+not+appear+&gs_l=psy-ab.3...9064.12569.0.12714.3.3.0.0.0.0.177.398.0j3.3.0....0...1c.1.64.psy-ab..0.0.0....0.WaBAx8lSpHY
+    // https://medium.com/xcblog/core-data-with-swift-4-for-beginners-1fc067cca707
+    //https://cocoacasts.com/setting-up-the-core-data-stack-with-nspersistentcontainer
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
